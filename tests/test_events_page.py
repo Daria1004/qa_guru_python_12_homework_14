e1973da3c090c6119d8c110291380c6fda5a5999
Daria Jakuszewicz <d.jakuszewicz@gmail.com>
2024-05-11 11:31:31 +0300

    Разметка with allure.step вынесена в класс

--- tests/test_events_page.py
+++ tests/test_events_page.py
@@ -1,28 +1,21 @@
-import allure, time
+import time
 from pages.events_page import events_page
 
 
 def test_get_events(open_events_page):
-    with allure.step("Открытие  модального окна Получить материалы"):
-        events_page.modal_should_have_title('Получить материалы')
+    events_page.modal_should_have_title('Получить материалы')
 
 
 def test_send_empty_form(open_events_page):
-    with allure.step("Отправить заявку на получение материалов"):
-        events_page.send_request_for_get_materials()
-    with allure.step("Получено сообщение об ошибке"):
-        events_page.get_error_message('Пожалуйста, заполните все обязательные поля')
+    events_page.send_request_for_get_materials()
+    events_page.get_error_message('Пожалуйста, заполните все обязательные поля')
 
 
 def test_send_fill_form(open_events_page):
-    with allure.step("Отправить заявку на получение материалов"):
-        events_page.send_request_for_get_materials()
-    with allure.step("Заполнение формы"):
-        events_page.fill_form()
-    with allure.step("Кликнуть чекбокс персональные данные"):
-        events_page.click_pers_info()
-    with allure.step("Отправить заявку на получение материалов"):
-        events_page.send_request_for_get_materials()
+    events_page.fill_form()
+    events_page.click_pers_info()
+    events_page.send_request_for_get_materials()
+    events_page.successful_redirect()
     # time.sleep(3)
     # with allure.step("Сообщение об успешном доступе к вебинару"):
     #     events_page.successful_redirect()
