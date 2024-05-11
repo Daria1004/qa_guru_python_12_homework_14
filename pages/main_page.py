9afdf74e3f01503286b196d8a24c8b7585482c6d
Daria Jakuszewicz <d.jakuszewicz@gmail.com>
2024-05-11 11:28:08 +0300

    Разметка with allure.step вынесена в класс

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,55 +1,75 @@
 import allure
 from selene import browser, have, command, be
 
+
 class MainPage:
     header = browser.element('#t-header')
     footer = browser.element('#t-footer')
-
 
     def open(self):
         with allure.step("Открытие главной страницы"):
             browser.open('/')
 
     def footer_should_have_text(self, value):
-        self.footer.perform(command.js.scroll_into_view)
-        self.footer.should(have.text(value))
+        with allure.step("Проверка наличия текста подвала"):
+            self.footer.perform(command.js.scroll_into_view)
+            self.footer.should(have.text(value))
 
     def open_feedback_modal(self):
-        browser.element('[href="#popup:myform-dms"]').perform(command.js.click)
+        with allure.step("Открытие  модального окна Оставить заявку"):
+            browser.element('[href="#popup:myform-dms"]').perform(command.js.click)
 
     def modal_should_have_title(self, value):
-        browser.element('[field="tn_text_1470209944682"]').should(have.text(value))
+        with allure.step("Проверка заголовка модального окна"):
+            value = value.replace('<br>', '\n').strip()
+            browser.element('[field="tn_text_1470209944682"]').should(have.text(value))
 
     def go_to_events(self):
-        browser.all("a").element_by(have.exact_text("События")).click()
+        with allure.step("Переход в меню События"):
+            browser.all("a").element_by(have.exact_text("События")).click()
 
     def should_have_upper_menu(self):
-        self.header.should(be.visible)
-        self.header.element('[data-elem-id="1663269457691"]').element('a').should(have.attribute('href', 'https://bestdoctor.ru/dms/'))
-        self.header.element('[data-elem-id="1680249993768"]').element('a').should(have.attribute('href', 'https://bestdoctor.ru/about/'))
-        self.header.element('[data-elem-id="1653047602052"]').element('a').should(have.attribute('href', 'https://bestdoctor.ru/events/'))
-        self.header.element('[data-elem-id="1653048299711"]').element('a').should(have.attribute('href', 'https://blog.bestdoctor.ru/'))
+        with allure.step("Проверка корректной навигации вверху страницы"):
+            self.header.should(be.visible)
+            self.header.element('[data-elem-id="1663269457691"]').element('a').should(
+                have.attribute('href', 'https://bestdoctor.ru/dms/'))
+            self.header.element('[data-elem-id="1680249993768"]').element('a').should(
+                have.attribute('href', 'https://bestdoctor.ru/about/'))
+            self.header.element('[data-elem-id="1653047602052"]').element('a').should(
+                have.attribute('href', 'https://bestdoctor.ru/events/'))
+            self.header.element('[data-elem-id="1653048299711"]').element('a').should(
+                have.attribute('href', 'https://blog.bestdoctor.ru/'))
 
     def should_have_info(self):
-        browser.element.element('[data-elem-id="1652883525054"]').should(have.text('Когда страховая — главный эксперт в ДМС'))
+        browser.element.element('[data-elem-id="1652883525054"]').should(
+            have.text('Когда страховая — главный эксперт в ДМС'))
         browser.element.element('[data-elem-id="1652883525054"]').should(have.text('Когда ДМС удобно пользоваться'))
-        browser.element.element('#sbs-473291340-1652883525054').should(have.text('Когда HR свободен от рутины и проблем'))
+        browser.element.element('#sbs-473291340-1652883525054').should(
+            have.text('Когда HR свободен от рутины и проблем'))
         browser.element.element('#sbs-473291343-1652883525054').should(have.text('Когда сотрудники довольны сервисом'))
 
     def should_have_social_medias(self):
-        self.footer.perform(command.js.scroll_into_view)
-        self.footer.should(be.visible)
-        self.footer.element('[data-elem-id="1690544179272"]').element('a').should(have.attribute('href', 'https://vc.ru/bestdoctor'))
-        self.footer.element('[data-elem-id="1690544192637"]').element('a').should(have.attribute('href', 'https://vk.com/bestdoctor'))
-        self.footer.element('[data-elem-id="1605874985072"]').element('a').should(have.attribute('href', 'https://t.me/bestdoctormedia'))
-        self.footer.element('[data-elem-id="1690544762473"]').element('a').should(have.attribute('href', 'https://ok.ru/group/70000003501952'))
-        self.footer.element('[data-elem-id="1705913572432"]').element('a').should(have.attribute('href', 'https://dzen.ru/bestdoctor'))
+        with allure.step("Проверка наличия ссылок на социальные сети в футере"):
+            self.footer.perform(command.js.scroll_into_view)
+            self.footer.should(be.visible)
+            self.footer.element('[data-elem-id="1690544179272"]').element('a').should(
+                have.attribute('href', 'https://vc.ru/bestdoctor'))
+            self.footer.element('[data-elem-id="1690544192637"]').element('a').should(
+                have.attribute('href', 'https://vk.com/bestdoctor'))
+            self.footer.element('[data-elem-id="1605874985072"]').element('a').should(
+                have.attribute('href', 'https://t.me/bestdoctormedia'))
+            self.footer.element('[data-elem-id="1690544762473"]').element('a').should(
+                have.attribute('href', 'https://ok.ru/group/70000003501952'))
+            self.footer.element('[data-elem-id="1705913572432"]').element('a').should(
+                have.attribute('href', 'https://dzen.ru/bestdoctor'))
 
     def switch_to_vc(self):
-        browser.element('[data-elem-id="1663269457691"]').click()
+        with allure.step("Переход на на платформу vc"):
+            browser.element('[data-elem-id="1663269457691"]').click()
 
     def open_vc(self):
-        browser.open("https://vc.ru/bestdoctor")
+        with allure.step("Открытие страницы bestdoctor на платформе vc"):
+            browser.open("https://vc.ru/bestdoctor")
 
 
 main_page = MainPage()
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,36 +1,26 @@
-import allure
 from pages.main_page import main_page
 from pages.events_page import events_page
 
 
 def test_open_main_page(open_main_page):
-    with allure.step("Проверка наличия текста подвала"):
-        main_page.footer_should_have_text("© 2015 — 2023 ООО «БЕСТДОКТОР»")
+    main_page.footer_should_have_text("© 2015 — 2023 ООО «БЕСТДОКТОР»")
 
 
 def test_feedback_modal(open_main_page):
-    with allure.step("Открытие  модального окна Оставить заявку"):
-        main_page.open_feedback_modal()
-    with allure.step("Проверка заголовка модального окна"):
-        main_page.modal_should_have_title('Заполните форму<br>для подключения компании')
+    main_page.open_feedback_modal()
+    main_page.modal_should_have_title('Заполните форму<br>для подключения компании')
 
 
 def test_header_menu(open_main_page):
-    with allure.step("Проверка корректной навигации вверху страницы"):
-        main_page.should_have_upper_menu()
+    main_page.should_have_upper_menu()
 
 
 def test_go_to_events(open_main_page):
-    with allure.step("Переход в меню События"):
-        main_page.go_to_events()
-    with allure.step("Открытие страницы Прошедшие вебинары"):
-        events_page.should_have_title("Прошедшие вебинары")
+    main_page.go_to_events()
+    events_page.should_have_title("Прошедшие вебинары")
 
 
 def test_integration_vc(open_main_page):
-    with allure.step("Проверка наличия ссылок на социальные сети в футере"):
-        main_page.should_have_social_medias()
-    with allure.step("Переход на на платформу vc"):
-        main_page.switch_to_vc()
-    with allure.step("Открытие страницы bestdoctor на платформе vc"):
-        main_page.open_vc()
+    main_page.should_have_social_medias()
+    main_page.switch_to_vc()
+    main_page.open_vc()
