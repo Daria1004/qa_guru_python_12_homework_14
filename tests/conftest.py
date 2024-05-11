9f6ebc2eb1d531a653fc092fe479263aa413486a
Daria Jakuszewicz <d.jakuszewicz@gmail.com>
2024-05-11 10:49:51 +0300

    исправлены фикстуры, добавлен селеноид

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,37 +1,74 @@
-import allure
+import os
 import pytest
 from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from utils import attach
+from dotenv import load_dotenv
+from pages.events_page import events_page
+from pages.main_page import main_page
 
-from pages.events_page import EventsPage
-from pages.main_page import MainPage
+DEFAULT_BROWSER_VERSION = '100.0'
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_management():
+def setup_browser(request, load_env):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
     browser.config.base_url = 'https://bestdoctor.ru'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
-    yield
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
 
-@pytest.fixture(scope='function', autouse=True)
+
+@pytest.fixture(scope='function')
 def open_events_page():
-    events_page = EventsPage()
-    with allure.step("Открытие страницы Прошедшие вебинары"):
-        events_page.open()
-    with allure.step("Получить запись"):
-        events_page.open_modal_get_record()
-
-    yield events_page
+    events_page.open()
+    events_page.open_modal_get_record()
 
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope='function')
 def open_main_page():
-    main_page = MainPage()
-    with allure.step("Открытие главной страницы"):
-        main_page.open()
-
-    yield main_page
+    main_page.open()
 
